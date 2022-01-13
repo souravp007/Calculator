6c952d152ad057ffc347eb2f45fe44961af14db5
RajniRani21 <rajnichaudhary762@gmail.com>
2022-01-13 14:19:07 +0530

    javascript file

---
+++ calci.js
@@ -0,0 +1,27 @@
+// addition function
+function add()
+{
+    i3.value=parseInt(i1.value)+parseInt(i2.value);
+}
+////subtract function
+function sub()
+{
+    i3.value=parseInt(i1.value)-parseInt(i2.value);
+
+}
+//multiply function
+function mul()
+{
+    i3.value=parseInt(i1.value)*parseInt(i2.value);
+    
+}
+
+// operator and numbers
+function show(a)
+{
+    t1.value=t1.value+a;
+}
+function  result()
+{
+    t1.value=eval(t1.value);
+}
