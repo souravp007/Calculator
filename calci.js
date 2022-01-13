22f60735d74138d1dd21bd2dfd73fbe82b596c83
souravp007 <souravpurohit7@gmail.com>
2022-01-13 15:52:52 +0530

    sourav has made changes to html and  js file

--- calci.js
+++ calci.js
@@ -1,27 +1,15 @@
-// addition function
-function add()
-{
-    i3.value=parseInt(i1.value)+parseInt(i2.value);
-}
-////subtract function
-function sub()
-{
-    i3.value=parseInt(i1.value)-parseInt(i2.value);
 
-}
-//multiply function
-function mul()
-{
-    i3.value=parseInt(i1.value)*parseInt(i2.value);
-    
-}
-
-// operator and numbers
 function show(a)
 {
     t1.value=t1.value+a;
 }
-function  result()
+
+function show1()
 {
-    t1.value=eval(t1.value);
+ t1.value=eval(t1.value)
+
+}
+function show2()
+{
+t1.value=""
 }
